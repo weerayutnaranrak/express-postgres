0ed1531b70bc3b79b29244e582735077fa2a70e3
Stamp <weerayut.naranrak@gmail.com>
2022-10-10 15:29:17 +0700

    fixed untracked files

--- dist/constants/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API_MEDTHOD = void 0;
-exports.API_MEDTHOD = {
-    GET: 'get',
-    POST: 'post'
-};
-//# sourceMappingURL=api.js.map
--- dist/routes/lead/getLead/validator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_validator_1 = require("express-validator");
-function registerEmailValidator() {
-    return [(0, express_validator_1.query)('limit').notEmpty().withMessage('Limit is required')];
-}
-exports.default = registerEmailValidator;
-//# sourceMappingURL=validator.js.map
